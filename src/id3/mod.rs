0c6e55bfb3d789a24bd6f4ff88bcd85254ad82e4
Richard McCormack <brick@brick.codes>
2018-07-11 22:59:30 -0400

    remove unnecessary &

--- src/id3/mod.rs
+++ src/id3/mod.rs
@@ -177,7 +177,7 @@ impl Parser {
                assert!(false);
             }
             let text_data = unsafe {
-               &std::slice::from_raw_parts(text_data.as_ptr() as *const u16, text_data.len() / 2)
+               std::slice::from_raw_parts(text_data.as_ptr() as *const u16, text_data.len() / 2)
             };
             Ok(Cow::from(String::from_utf16(text_data)?))
          } // UTF 16 with BOM
