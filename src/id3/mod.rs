2d7c9761cbaba67d7faa0b4e129588773489d945
Richard McCormack <brick@brick.codes>
2019-01-19 00:56:02 -0500

    fix botched extended header implementation

--- src/id3/mod.rs
+++ src/id3/mod.rs
@@ -65,40 +65,17 @@ pub fn parse_source<S: Read + Seek>(source: &mut S) -> Result<Parser, TagParseEr
             unimplemented!();
          }
 
+         // TODO: for performance, we might be able to get away with wrapping sub
+         // because we have to do bound checks later anyway
          if flags.contains(v24::TagFlags::EXTENDED_HEADER) {
-            let eh_size = synchsafe_u32_to_u32(source.read_u32::<BigEndian>()?)
-               // eh_size includes itself
-               .saturating_sub(4);
-            let mut eh_bytes = vec![0u8; eh_size as usize].into_boxed_slice();
+            let eh_size = synchsafe_u32_to_u32(source.read_u32::<BigEndian>()?);
+
+            size_of_frames = size_of_frames.saturating_sub(eh_size);
+            // we have to make sure to sub 4, as eh_size includes itself
+            let mut eh_bytes = vec![0u8; eh_size.saturating_sub(4) as usize].into_boxed_slice();
             source.read_exact(&mut eh_bytes)?;
-            debug_assert_eq!(eh_bytes[0], 1); // Number of flag bytes
-
             // eh_bytes[0] is always (supposed to be) set to 1
-            let eh_flags = v24::ExtendedHeaderFlags::from_bits_truncate(eh_bytes[1]);
-
-            size_of_frames = size_of_frames.saturating_sub(6);
-
-            if eh_flags.contains(v24::ExtendedHeaderFlags::TAG_IS_UPDATE) {
-               let _ = source.read_u8(); // 0
-            }
-
-            if eh_flags.contains(v24::ExtendedHeaderFlags::CRC_DATA_PRESENT) {
-               let _ = source.read_u8(); // 5
-
-               let mut crc_bytes = [0; 5];
-               source.read_exact(&mut crc_bytes)?;
-               // TODO: do something with this? and other EH FLAGS?
-               // note to future self: haven't dealt with endianness of crc_bytes yet
-               size_of_frames = size_of_frames.saturating_sub(5);
-            }
-
-            if eh_flags.contains(v24::ExtendedHeaderFlags::TAG_RESTRICTIONS) {
-               let _ = source.read_u8(); // 1
-
-               // not really sure why we care, is there any point in obeying these restrictions?
-               let _restrictions = source.read_u8();
-               size_of_frames = size_of_frames.saturating_sub(1);
-            }
+            let _eh_flags = v24::ExtendedHeaderFlags::from_bits_truncate(eh_bytes[1]);
          }
 
          if flags.contains(v24::TagFlags::EXPERIMENTAL_INDICATOR) {
